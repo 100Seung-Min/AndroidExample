641234b77af8b3a8245b42463843bea365874b4d
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-08-23 10:33:52 +0900

    [Video] BaseActivity / BaseFragment

---
+++ VideoExample/app/src/main/java/com/example/videoexample/presentation/ui/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.videoexample.presentation.ui
+
+import com.example.videoexample.R
+import com.example.videoexample.databinding.ActivityMainBinding
+import com.example.videoexample.presentation.ui.base.BaseActivity
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun init() {
+
+    }
+}
