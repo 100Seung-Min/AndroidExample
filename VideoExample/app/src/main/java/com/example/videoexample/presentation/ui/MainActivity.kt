690eda4b86939e805faf53d3c566ae7de9b74cfc
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-08-23 10:35:58 +0900

    [Video] Hilt Setting

--- VideoExample/app/src/main/java/com/example/videoexample/presentation/ui/MainActivity.kt
+++ VideoExample/app/src/main/java/com/example/videoexample/presentation/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.videoexample.presentation.ui
 import com.example.videoexample.R
 import com.example.videoexample.databinding.ActivityMainBinding
 import com.example.videoexample.presentation.ui.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun init() {
 
