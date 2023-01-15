9928c01927e321a6200cdc2b6a126e5e4344d97b
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-01-15 19:49:39 +0900

    [Photo]dataBinding setting

--- PhotoExample/app/src/main/java/com/example/photoexample/MainActivity.kt
+++ PhotoExample/app/src/main/java/com/example/photoexample/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.photoexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.photoexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun onClick(view: View) {
+        when (view) {
+            binding.outerPhotoBtn -> {
+            }
+        }
     }
 }
