4ea5e7ed8a5f9e6fafe42ac6079a59c0c597b252
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-03-21 11:24:33 +0900

    [Push] FCMToken 조회

--- PushExample/app/src/main/java/com/example/pushexample/MainActivity.kt
+++ PushExample/app/src/main/java/com/example/pushexample/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.pushexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { token ->
+                if (token.isSuccessful) {
+                    println("안녕 ${token.result}")
+                }
+            }
     }
 }
